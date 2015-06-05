38b84ba2a15bf5747aba4329e00a883410e52749
Afshin Mehrabani <afshin.meh@gmail.com>
2015-06-05 09:48:26 +0430

    update clientside

--- client/pw.js
+++ client/pw.js
@@ -12,17 +12,54 @@
     if (typeof (window.pwroutes) != 'object') throw new Error('no pwroutes found.');
 
     for (var i in window.pwroutes) {
-      var route = window.pwroutes[i];
-
-      page(route.path, handler);
+      page(window.pwroutes[i].path, handler);
     };
 
     page();
   };
 
-  function handler () {
-    console.log('here we go');
-    console.log(this)
+  function handler (ctx) {
+    loading(true);
+
+    var route = getRoute(ctx.path);
+
+    if (route != null) {
+      var client = new WebTorrent();
+      var hash = route.hash;
+
+      client.add(hash, function (torrent) {
+        var file = torrent.files[0];
+
+        file.getBuffer(function (err, buffer) {
+          var container = document.querySelector('#container');
+          container.innerHTML = buffer.toString();
+          loading(false);
+        });
+      });
+    }
+  };
+
+  function getRoute (path) {
+    for (var i in window.pwroutes) {
+      var route = window.pwroutes[i];
+
+      if (route.path == path) {
+        return route;
+        break;
+      }
+    };
+
+    return null;
+  };
+
+  function loading (enable) {
+    if (enable) {
+      NProgress.start();
+      var container = document.querySelector('#container');
+      container.innerHTML = '<div class="one-half column" style="margin-top: 25%"><h4>Rocket science. Please wait...</h4></div>';
+    } else {
+      NProgress.done();
+    }
   };
 
   //start watching
